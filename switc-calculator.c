66246ea3d034ec7aaad5ddf8e23898b50e0f6f5c
LuisMezarina <diegomezarina.provincias@gmail.com>
2026-02-12 23:20:49 -0500

    Ejercicio completado

--- switc-calculator.c
+++ switc-calculator.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    int numero1=0, numero2=0, continuar=-1, respuesta=-1;
+    int numero1=0, numero2=0, continuar=-1, respuesta=-1, suma=0, resta=0, multiplicacion=0, division=0;
 
     while (continuar==-1)
     {
@@ -22,10 +22,10 @@ int main()
 
         switch (respuesta)
         {
-        case 1: ;break; 
-        case 2: ;break; 
-        case 3: ;break;
-        case 4: ;break;
+        case 1: suma=numero1+numero2; printf("suma:%d\n", suma) ;break; 
+        case 2: resta=numero1-numero2; printf("Resta:%d\n", resta);break; 
+        case 3: multiplicacion=numero1*numero2; printf("Multiplicacion:%d\n", multiplicacion) ;break;
+        case 4: division=numero1/numero2; printf("Division:%d\n", division) ;break;
         case 5: continuar=0 ;break;
         default: printf("Opcion no valida\n") ;break; 
         }
